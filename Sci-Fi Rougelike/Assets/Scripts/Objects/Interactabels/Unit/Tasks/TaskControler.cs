7815b5fddca47954461e76ba3f466db144f9507f
Griizz <griizz-development@gmx.de>
2017-09-09 22:28:35 +0200

    more basics added

---
+++ Sci-Fi Rougelike/Assets/Scripts/Objects/Interactabels/Unit/Tasks/TaskControler.cs
@@ -0,0 +1,114 @@
+﻿//Copyright © Darwin Willers 2017
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(UnitMover), typeof(UnitRotater),  typeof(UnitInteractor))]
+public class TaskControler : MonoBehaviour
+{
+
+
+    public bool TaskInProgress { get; private set; }
+
+    private Queue<Task> _taskQ;
+
+    private UnitMover _unitMover;
+    private UnitRotater _unitRotater;
+    private UnitInteractor _unitInteractor;
+    private Task _currentTask;
+
+    private void Start()
+    {
+        _taskQ = new Queue<Task>();
+        
+        _unitMover = GetComponent<UnitMover>();
+        _unitRotater = GetComponent<UnitRotater>();
+        _unitInteractor = GetComponent<UnitInteractor>();
+
+        _unitMover.taskDone += TaskDone;
+        _unitRotater.taskDone += TaskDone;
+        _unitInteractor.taskDone += TaskDone;
+    }
+
+    #region AddTask - Functions
+    public void AddTask(Task task)
+    {
+        _taskQ.Enqueue(task);
+    }
+
+    public void AddTask(TaskType type, Vector3 pos)
+    {
+        AddTask( new Task(type, pos));
+    }
+
+    public void AddTask(TaskType type, params Vector3[] posArray)
+    {
+        foreach (var pos in posArray)
+        {
+            AddTask(new Task(type, pos));
+        }
+    }
+    
+    public void AddTask(TaskType type1, TaskType type2, params Vector3[] posArray)
+    {
+        foreach (var pos in posArray)
+        {
+            AddTask(new Task(type1, pos));
+            AddTask(new Task(type2, pos));
+        }
+    }
+    
+    /// <summary>
+    /// Automaticly Adds a LookAt and then a Move Task for each position
+    /// </summary>
+    /// <param name="posArray"></param>
+    public void AddTask(params Vector3[] posArray)
+    {
+        foreach (var pos in posArray)
+        {
+            AddTask(new Task(TaskType.LookAt, pos));
+            AddTask(new Task(TaskType.Move, pos));
+        }
+    }
+    
+    
+    #endregion
+
+    private void TaskDone()
+    {
+        TaskInProgress = false;
+    }
+
+    private void Update()
+    {
+        if (!TaskInProgress && _taskQ.Count != 0)
+            StartNextTask();
+    }
+
+    private void StartNextTask()
+    {
+        _currentTask = _taskQ.Dequeue();
+        TaskInProgress = true;
+        
+        switch(_currentTask.type)
+        {
+            case TaskType.LookAt:
+                _unitRotater.LookAt(_currentTask.position);
+                break;
+                
+            case TaskType.Move:
+                _unitMover.MoveUnit(_currentTask.position);
+                break;
+                
+            case TaskType.Interact:
+                _unitInteractor.Interact(_currentTask.position);
+                break;
+                
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+}
---
+++ Sci-Fi Rougelike/Assets/Scripts/Objects/Interactabels/Unit/Tasks/UnitRotater.cs
@@ -0,0 +1,56 @@
+﻿//Copyright © Darwin Willers 2017
+
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class UnitRotater : MonoBehaviour
+{
+
+    public Action taskDone;
+    
+    public float rotationTime = .5f;
+    public bool IsRotating { get; private set; }
+
+    public void LookAt(Vector3 target)
+    {
+        if (CheckIsRotating()) return;
+
+        StartCoroutine(LookAt(transform, target, rotationTime));
+    }
+
+    private IEnumerator LookAt(Transform unit, Vector3 target, float time)
+    {
+        IsRotating = true;
+        
+        float timePassed = 0;
+        target.y = unit.position.y;
+        var startRotation = unit.rotation;
+        unit.LookAt(target);
+        var endRotation = unit.rotation;
+        unit.rotation = startRotation;
+
+        if (startRotation != endRotation)
+        {
+            while (timePassed < rotationTime)
+            {
+                timePassed += Time.deltaTime;
+                unit.rotation = Quaternion.Lerp(startRotation, endRotation, timePassed / time);
+                yield return null;
+            }
+            
+            unit.rotation = endRotation;
+        }
+
+        IsRotating = false;
+        if (taskDone != null) taskDone();
+    }
+    
+    private bool CheckIsRotating()
+    {
+        if (!IsRotating) return false;
+        Debug.LogError("UnitRot cator of: " + gameObject.name + " recieved multiple LookAt calls");
+        return true;
+    }
+}
